1c511d2882b16edb3b31c87579cbc6d4bc0a2d63
dldl1901s <75300297+dldl1901s@users.noreply.github.com>
2022-06-03 00:31:04 -0700

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken =
     'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
 let map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
     projection: {
         name: 'albers'
     },
@@ -11,49 +11,51 @@ let map = new mapboxgl.Map({
 });
 
 const colorstop = [
-    [0, 'rgb(247,251,255)'],
-    [30, 'rgb(158,202,225)'],
-    [70, 'rgb(66,146,198)'],
-    [100, 'rgb(33,113,181)'],
-    [130, 'rgb(8,48,107)']
+    [100, 'rgb(247,251,255)'],
+    [1000, 'rgb(158,202,225)'],
+    [10000, 'rgb(66,146,198)'],
+    [30000, 'rgb(33,113,181)'],
+    [60000, 'rgb(8,48,107)']
 ];
 
+const sizestop = [
+    [100, 3],
+    [1000, 7],
+    [10000, 11],
+    [30000, 15],
+    [60000, 19]
+]
+
 map.on('load', () => {
-    map.addSource('rate', {
+    map.addSource('counts', {
         type: 'geojson',
-        data: 'assets/us-covid-2020-rates.json'
+        data: 'assets/us-covid-2020-counts.json'
     });
 
     map.addLayer({
-        'id': 'rate-layer',
-        'type': 'fill',
-        'source': 'rate',
+        'id': 'counts-point',
+        'type': 'circle',
+        'source': 'counts',
         'minzoom': 3,
         'paint': {
-            'fill-color': {
-                'property': 'rates',
+            'circle-radius': {
+                'property': 'cases',
+                'stops': sizestop
+            },
+            'circle-color': {
+                'property': 'cases',
                 'stops': colorstop
             },
-            'fill-opacity': 0.5
+            'circle-stroke-color': 'black',
+            'circle-stroke-width': 1,
+            'circle-opacity': 0.8
         }
     });
 
-    map.addLayer({
-        'id': 'outline',
-        'type': 'line',
-        'source': 'rate',
-        'paint': {
-            'line-color': 'white',
-            'line-width': 0.3,
-            'line-opacity': 0.7
-        }
-    });
-
-
-    map.on('click', 'rate-layer', (event) => {
+    map.on('click', 'counts-point', (event) => {
         new mapboxgl.Popup()
-            .setLngLat(event.lngLat)
-            .setHTML(`<strong>County:</strong> ${event.features[0].properties.county}<br><strong>Rates:</strong> ${event.features[0].properties.rates}%`)
+            .setLngLat(event.features[0].geometry.coordinates)
+            .setHTML(`<strong>County:</strong> ${event.features[0].properties.county}<br><strong>Cases:</strong> ${event.features[0].properties.cases}`)
             .addTo(map);
     });
 
@@ -62,16 +64,18 @@ map.on('load', () => {
 const legend = document.getElementById('legend');
 
 //set up legend grades and labels
-var labels = ['<strong>Percentages</strong>'],
+var labels = ['<strong>Size and Cases</strong>'],
     vbreak;
 //iterate through grades and create a scaled circle and label for each
 for (var i = 0; i < 5; i++) {
-    vbreak = colorstop[i][0];
+    vbreak = sizestop[i][0];
     // you need to manually adjust the radius of each dot on the legend 
     // in order to make sure the legend can be properly referred to the dot on the map.
     labels.push(
-        '<p class="break"><i class="rectangle" style="background:' + colorstop[i][1] + '; width: 25px; height: 25px;"></i> <span class="dot-label" style="top: 10px;">' + vbreak +
-        '% </span></p>');
+        '<p class="break"><i class="dot" style="background:' + colorstop[i][1] + '; width: ' + sizestop[i][1]*2.1 +
+        'px; height: ' +
+        sizestop[i][1]*2.1 + 'px; "></i> <span class="dot-label" style="top: ' + sizestop[i][1] + 'px;">' + vbreak +
+        '</span></p>');
 }
 const source1 =
 '<p style="text-align: right;  font-size:8pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">The New York Times,</a><br></p>';
